5d62d6b35a501fd9d07eed28a568d135946e1ab9
Georgiy Kamnev <nt.gocha@gmail.com>
2024-01-11 14:12:27 +0500

    fix

--- coll-im/deploy2central.sh
+++ coll-im/deploy2central.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED"
-mvn deploy
+mvn -P pub-sonatype deploy
