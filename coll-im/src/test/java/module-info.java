c768ebc2636b6b8c2485d7e7004eaa6d3ce7f00c
Georgiy Kamnev <nt.gocha@gmail.com>
2024-07-24 17:37:34 +0500

    ver 3.0

--- coll-im/src/test/java/module-info.java
+++ coll-im/src/test/java/module-info.java
@@ -1,8 +1,5 @@
 open module xyz.cofe.coll.im.test {
     exports xyz.cofe.coll.im.test;
-    requires transitive com.fasterxml.jackson.annotation;
-    requires transitive com.fasterxml.jackson.core;
-    requires transitive com.fasterxml.jackson.databind;
 
     requires transitive org.junit.jupiter.api;
     requires transitive org.junit.jupiter.engine;
---
+++ readme.md
@@ -0,0 +1,7 @@
+Управление версиями
+
+    mvn versions:set -DnewVersion=3.0-SNAPSHOT
+
+    mvn versions:revert
+
+    mvn versions:commit
